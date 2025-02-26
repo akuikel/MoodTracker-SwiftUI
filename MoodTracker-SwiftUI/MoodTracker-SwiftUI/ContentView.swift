9928a4506e708fdc927e6fe6743e071bae581a06
Aavash Kuikel <aavashkuikel@gmail.com>
2025-02-26 16:44:37 -0600

    initial xcode changes

---
+++ MoodTracker-SwiftUI/MoodTracker-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoodTracker-SwiftUI
+//
+//  Created by Aavash Kuikel on 2/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello. Commit number 1")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoodTracker-SwiftUI/MoodTracker-SwiftUI/MoodTracker_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTracker_SwiftUIApp.swift
+//  MoodTracker-SwiftUI
+//
+//  Created by Aavash Kuikel on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodTracker_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
